7e34f7b04f9a77df4272880a822987ae836675ed
Samuel Obeng <58854129+Samuel204@users.noreply.github.com>
2023-04-06 12:52:41 +0200

    Add files via upload

---
+++ Anni precedenti/2_C++/Auto.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+
+
+int main() {
+
+    //tipo id;
+    int a;
+    double b;
+    double c=7.5;
+
+    //for(std::vector<double> iterator it=v.begin()) // andro sostituire con auto al posto di scrivere std::vector<double>
+
+    return 0;
+}
